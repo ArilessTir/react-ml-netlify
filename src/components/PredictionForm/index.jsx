c31e9a8b219b8832b3b667325a6c3c9a57a84fe7
ArilessTir <ariless.tir@gmail.com>
2021-07-20 15:12:46 +0200

    handle change func

--- src/components/PredictionForm/index.jsx
+++ src/components/PredictionForm/index.jsx
@@ -1,5 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-const PredictionForm = () => <form />;
+const PredictionForm = () => {
+  const [data, setData] = useState({
+    PassengerId: 343,
+    Pclass: 2,
+    Name: "Collander, Mr. Erik Gustaf",
+    Sex: "male",
+    Age: 28.0,
+    SibSp: 0,
+    Parch: 0,
+    Ticket: "248740",
+    Fare: 13.0,
+    Cabin: null,
+    Embarked: "S",
+  });
+
+  const handleChange = (key, value) => {
+    setData((obj) => ({
+      ...obj,
+      [key]: value,
+    }));
+  };
+
+  return <div />;
+};
 
 export default PredictionForm;
