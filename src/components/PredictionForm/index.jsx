4539004c08e3a1762786800ae118071b09ada937
ArilessTir <ariless.tir@gmail.com>
2021-07-21 16:42:23 +0200

    add form

--- src/components/PredictionForm/index.jsx
+++ src/components/PredictionForm/index.jsx
@@ -1,6 +1,11 @@
+/* eslint-disable no-unused-vars */
 import React, { useState } from "react";
+import axios from "axios";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import PropTypes from "prop-types";
 
-const PredictionForm = () => {
+// eslint-disable-next-line react/prop-types
+const PredictionForm = ({ childToParent }) => {
   const [data, setData] = useState({
     PassengerId: 343,
     Pclass: 2,
@@ -11,10 +16,9 @@ const PredictionForm = () => {
     Parch: 0,
     Ticket: "248740",
     Fare: 13.0,
-    Cabin: null,
+    Cabin: "PC52",
     Embarked: "S",
   });
-
   const handleChange = (key, value) => {
     setData((obj) => ({
       ...obj,
@@ -22,7 +26,121 @@ const PredictionForm = () => {
     }));
   };
 
-  return <div />;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    childToParent("loading");
+    setTimeout(() => {
+      axios
+        .post("https://mytitanicapipred.herokuapp.com/predict", data)
+        .then((res) => {
+          console.log(res);
+          childToParent(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }, 3000);
+  };
+
+  return (
+    <form className="block">
+      <input
+        placeholder="Pclass"
+        type="text"
+        defaultValue={data.Pclass}
+        onChange={(e) => {
+          e.preventDefault();
+          handleChange("Pclass", e.target.value);
+        }}
+      />
+      <input
+        placeholder="Name"
+        type="text"
+        defaultValue={data.Name}
+        onChange={(e) => {
+          e.preventDefault();
+          handleChange("Name", e.target.value);
+        }}
+      />
+      <input
+        placeholder="Sex"
+        type="text"
+        defaultValue={data.Sex}
+        onChange={(e) => {
+          e.preventDefault();
+          handleChange("Sex", e.target.value);
+        }}
+      />
+      <input
+        placeholder="Age"
+        type="text"
+        defaultValue={data.Age}
+        onChange={(e) => {
+          e.preventDefault();
+          handleChange("Age", e.target.value);
+        }}
+      />
+      <input
+        placeholder="SibSp"
+        type="text"
+        defaultValue={data.SibSp}
+        onChange={(e) => {
+          e.preventDefault();
+          handleChange("SibSp", e.target.value);
+        }}
+      />
+      <input
+        placeholder="Parch"
+        type="text"
+        defaultValue={data.Parch}
+        onChange={(e) => {
+          e.preventDefault();
+          handleChange("Parch", e.target.value);
+        }}
+      />
+      <input
+        placeholder="Ticket"
+        type="text"
+        defaultValue={data.Ticket}
+        onChange={(e) => {
+          e.preventDefault();
+          handleChange("Ticket", e.target.value);
+        }}
+      />
+      <input
+        placeholder="Fare"
+        type="text"
+        defaultValue={data.Fare}
+        onChange={(e) => {
+          e.preventDefault();
+          handleChange("Fare", e.target.value);
+        }}
+      />
+      <input
+        placeholder="Cabin"
+        type="text"
+        defaultValue={data.Cabin}
+        onChange={(e) => {
+          e.preventDefault();
+          handleChange("Cabin", e.target.value);
+        }}
+      />
+      <input
+        placeholder="Embarked"
+        type="text"
+        defaultValue={data.Embarked}
+        onChange={(e) => {
+          e.preventDefault();
+          handleChange("Embarked", e.target.value);
+        }}
+      />
+      <input type="submit" onClick={handleSubmit} />
+    </form>
+  );
+};
+
+PredictionForm.prototype = {
+  childToParent: PropTypes.function,
 };
 
 export default PredictionForm;
--- src/pages/Prediction/index.jsx
+++ src/pages/Prediction/index.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/Navbar";
 import PredictionForm from "../../components/PredictionForm";
 
-const Prediction = () => (
-  <>
-    <Navbar />
-    <h1> Hello from pred form</h1>
-    <PredictionForm />
-  </>
-);
+const Prediction = () => {
+  const [result, setResult] = useState("");
+
+  const childToParent = (childData) => {
+    setResult(childData);
+  };
+
+  return (
+    <>
+      <Navbar />
+      <h1> Hello from pred form</h1>
+      <PredictionForm childToParent={childToParent} />
+      <h1>Result: {result} </h1>
+    </>
+  );
+};
 
 export default Prediction;
