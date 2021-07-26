4efc80bcd4fc2e391ee2d8a5c3cafba3e4cfa1b8
ArilessTir <ariless.tir@gmail.com>
2021-07-26 16:31:09 +0200

    prediction page

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,8 +3,8 @@ import React from "react";
 // eslint-disable-next-line react/prop-types
 const Card = ({ children }) => (
   <div
-    className="w-1/3 h-1/3 shadow-lg rounded-lg
-   absolute top-1/2 right-1/2 transform -translate-y-1/2"
+    className="w-2/3 h-2/5 shadow-lg rounded-lg
+   absolute top-1/2 left-1/2 transform -translate-y-2/3 -translate-x-1/2"
   >
     {children}
   </div>
--- src/pages/Prediction/index.jsx
+++ src/pages/Prediction/index.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Navbar from "../../components/Navbar";
 import PredictionForm from "../../components/PredictionForm";
 import Card from "../../components/Card";
+import Robot from "../../components/Robot";
 
 const Prediction = () => {
   const [result, setResult] = useState("");
@@ -13,11 +14,24 @@ const Prediction = () => {
   return (
     <>
       <Navbar />
-      <h1 className="text-center pt-16"> Hello from pred form</h1>
-      <Card>
-        <PredictionForm childToParent={childToParent} />
-      </Card>
-      <h1 className="text-center">Result: {result} </h1>
+      <body className="flex h-screen">
+        <div className="w-1/2  relative">
+          <div className="pb-32 absolute top-1/4 left-1/2 transform -translate-y-2/3 -translate-x-1/2 text-center text-xl">
+            {" "}
+            <h1> Ask the model to know if you gonna </h1>
+            <span className="text-blue-500 text-2xl"> Live </span> or{" "}
+            <span className=" text-2xl"> Die </span>
+            <h1 className="text-center">Result: {result} </h1>
+          </div>
+
+          <Card>
+            <PredictionForm childToParent={childToParent} />
+          </Card>
+        </div>
+        <div className="w-1/2 relative">
+          <Robot data={result} />
+        </div>
+      </body>
     </>
   );
 };
